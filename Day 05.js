33b35dafa7943562388200bc1853b9329ad04186
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2025-08-25 18:21:06 +0530

    Create Day 05.js

---
+++ Day 05.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const a = [];
+
+    for(let i = 0; i < arr.length; i++){
+        a.push(fn(arr[i], i));
+    }
+
+    return a;
+};
